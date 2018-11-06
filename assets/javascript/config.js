79bd52096c50b1eb932973c0bb522c1beb50cbd7
Adrienne Rice <adrienne.rice@gmail.com>
2018-11-05 21:38:57 -0500

    update gitignore

---
+++ assets/javascript/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyAPw4tOXUv8ckhfqzNCZvD0OOoerJtcNTM",
+    authDomain: "trainschedule-14c52.firebaseapp.com",
+    databaseURL: "https://trainschedule-14c52.firebaseio.com",
+    projectId: "trainschedule-14c52",
+    storageBucket: "trainschedule-14c52.appspot.com",
+    messagingSenderId: "802493873874"
+  };
