c1d173cd98ea8827e1892a76ef05647f44a43574
Adrienne Rice <adrienne.rice@gmail.com>
2018-11-05 22:07:44 -0500

    remove config file

--- assets/javascript/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-    apiKey: "AIzaSyAPw4tOXUv8ckhfqzNCZvD0OOoerJtcNTM",
-    authDomain: "trainschedule-14c52.firebaseapp.com",
-    databaseURL: "https://trainschedule-14c52.firebaseio.com",
-    projectId: "trainschedule-14c52",
-    storageBucket: "trainschedule-14c52.appspot.com",
-    messagingSenderId: "802493873874"
-  };
