5a1fdcf76c71fc6be8afee56b958b2712b181a1f
Adrienne Rice <adrienne.rice@gmail.com>
2018-11-05 20:51:48 -0500

    update js config

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,8 @@
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAPw4tOXUv8ckhfqzNCZvD0OOoerJtcNTM",
-    authDomain: "trainschedule-14c52.firebaseapp.com",
-    databaseURL: "https://trainschedule-14c52.firebaseio.com",
-    projectId: "trainschedule-14c52",
-    storageBucket: "trainschedule-14c52.appspot.com",
-    messagingSenderId: "802493873874"
-  };
-  firebase.initializeApp(config);
+var myConfig = config;
 
-  var database = firebase.database();
+firebase.initializeApp(myConfig);
+
+var database = firebase.database();
 
 // 2. Button for adding Employees
 $("#submit").on("click", function(event) {
